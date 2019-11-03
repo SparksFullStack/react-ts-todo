33ff3cf6d6d77c2fc4ef0abb074db28f5cafd557
Charlie Sparks <sparksfullstack@gmail.com>
2019-11-03 13:32:19 -0500

    added interfaces for actions and responses

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,14 +1,26 @@
 import axios from 'axios';
 import { Dispatch } from 'redux';
+import { ActionTypes } from './types';
+
+interface Todo {
+    id: number;
+    title: string;
+    completed: Boolean;
+};
+
+interface FetchTodosAction {
+    type: ActionTypes.fetchTodos;
+    payload: Todo[];
+}
 
 const reqUrl = 'http://jsonplaceholder.typicode.com/todos';
 
 export const fetchTodos = () => {
     return async (dispatch: Dispatch) => {
-        const response = await axios.get(reqUrl);
+        const response = await axios.get<Todo[]>(reqUrl);
 
-        dispatch({
-            type: 'FETCH_TODOS',
+        dispatch<FetchTodosAction>({
+            type: ActionTypes.fetchTodos,
             payload: response.data
         });
     };
